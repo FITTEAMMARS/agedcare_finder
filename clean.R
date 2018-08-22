1d5ba275a2eaac75f00b1502dea05198273b56f4
Unknown <nishant.deshpande19@gmail.com>
2018-08-22 18:55:50 +1000

    deploy test data
    some small test data has been generated for testing in website

--- clean.R
+++ clean.R
@@ -5,6 +5,8 @@ library(tm)
 library(stringr)
 library(tidyverse)
 library(tidytext)
+library(reshape2)
+
 
 #setwd("C:/Users/Nish/Documents/5120/agedcare_finder/data/")
 # aged care residential
@@ -26,17 +28,29 @@ ach$X.4 <- NULL
 # These aren't needed
 ach$MAX_EXIT_AMOUNT <- NULL
 
-## Hours worked
+
+# Time -----------------
 # opening hours
 open_hours_std <- unlist(lapply(X = ach$HOURS_STANDARD, FUN = str_extract, pattern = ".+?(?=\\s-)"))
+open_hours_std <- gsub("\\.", ":", open_hours_std)
+open_hours_std <- format(strptime(open_hours_std, "%I:%M %p"), format="%H:%M:%S")
 # closing hours
 close_hours_std <- unlist(lapply(X = ach$HOURS_STANDARD, FUN = str_extract, pattern = "(?<=-\\s).*"))
+close_hours_std <- gsub("\\.", ":", close_hours_std)
+close_hours_std <- format(strptime(close_hours_std, "%I:%M %p"), format="%H:%M:%S")
 
+# Address ------------
+# consistent state abbv
+ach[ach$STREET_STATE == "qld",]$STREET_STATE = "QLD"
+# re-factor
+ach$STREET_STATE = factor(ach$STREET_STATE)
+
+# NLP ------------
 ach$DESCRIPTION <- as.character(ach$DESCRIPTION) %>%
     removePunctuation() %>%
     tolower()
 
-# Weird column name
+# Weird column name(s)
 names(ach)[names(ach) == "ï..OUTLET_NAME"] <- "OUTLET_NAME"
 
 # description cleaning
@@ -74,3 +88,27 @@ create_address_string <- function(row_number, data_set){
   out <- paste(row$BUS_ST_ADDRESS, row$BUS_SUBURB, row$BUS_PCODE, sep = "")
   return(out)
 }
+
+# string AM/PM to 24h time 
+
+to_24h <- function(time_string){
+    return("pass")
+}
+
+
+# OUTPUT ------------
+
+export_home <- function(){
+    clean_data_home <- melt(data.frame(ach$OUTLET_NAME, 
+                                       open_hours_std, 
+                                       close_hours_std,
+                                       ach$STREET_STATE))
+    colnames(clean_data_home) <- c("OUTLET_NAME",
+                                   "HOURS_OPEN",
+                                   "HOURS_CLOSE",
+                                   "STATE")
+    write.csv(x = clean_data_home, file = "./data/clean/clean_data_home.csv", na = "NaN", row.names = FALSE)
+}
+
+export_home()
+
