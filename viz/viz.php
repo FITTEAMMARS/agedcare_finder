a1a6bf5d8327327523762a3151c9750cf5404ceb
Unknown <x.che01261027@gmail.com>
2018-09-17 12:31:22 +1000

    add php

---
+++ viz/viz.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: viz
+*/
+
+
+?>
+
+<head>
+    <style>
+        .headerbackground {
+            height: 80px;
+            background-color: #757575;
+        }
+
+        #highcharts-4e6ff2b0-e7fe-4f6e-a85f-edaf223a01a8 {
+            padding: 20px;
+        }
+
+    </style>
+</head>
+<?php
+get_header();
+?>
+<div class="headerbackground">
+</div>
+<div id="highcharts-0b4ee0d2-4cda-4a17-96f1-0a15bbbb04b8"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+
+<div id="highcharts-720ec5dd-fa44-48b5-9c99-967c929e8b6e"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<div id="highcharts-3a6ccf16-bb89-4568-8ca3-e2a11ad3d13a"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<div id="highcharts-819641f2-29aa-4426-8159-17297864e8ad"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<?php get_footer(); ?>
---
+++ website/viz.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: viz
+*/
+
+
+?>
+
+<head>
+    <style>
+        .headerbackground {
+            height: 80px;
+            background-color: #757575;
+        }
+
+        #highcharts-4e6ff2b0-e7fe-4f6e-a85f-edaf223a01a8 {
+            padding: 20px;
+        }
+
+    </style>
+</head>
+<?php
+get_header();
+?>
+<div class="headerbackground">
+</div>
+<div id="highcharts-0b4ee0d2-4cda-4a17-96f1-0a15bbbb04b8"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+
+<div id="highcharts-720ec5dd-fa44-48b5-9c99-967c929e8b6e"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<div id="highcharts-3a6ccf16-bb89-4568-8ca3-e2a11ad3d13a"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<div id="highcharts-e1e525c2-a665-4e08-80a8-d2d522bce5c6"></div>
+<br>
+<div class="needs" align="center">
+
+</div>
+<?php get_footer(); ?>
